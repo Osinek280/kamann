b76815bc9755d50cd82370bfa3cb94fd936a8c3a
Szymon <szymon.osinski.2009@gmail.com>
2025-01-10 23:15:07 +0100

    fix: hydration error

--- components/providers.tsx
+++ components/providers.tsx
@@ -6,6 +6,16 @@ import { ThemeProvider } from "next-themes"
 export function Providers({
   children,
 }: React.ComponentProps<typeof ThemeProvider>) {
+  const [mounted, setMounted] = React.useState(false)
+
+  React.useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
+
   return (
     <ThemeProvider
       attribute="class"
