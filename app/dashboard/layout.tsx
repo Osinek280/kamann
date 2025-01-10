fca6f7ff5ee8318bc7d5dffb504cf3ca1c37bdfb
Szymon <szymon.osinski.2009@gmail.com>
2025-01-10 23:13:32 +0100

    feat(): basic dashboard setup

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react"
+import DashboardSideBar from "./components/dashboard-side-bar"
+import DashboardTopNav from "./components/dashbord-top-nav"
+
+export default async function DashboardLayout({ children }: { children: ReactNode }) {
+
+  return (
+    <div className="grid min-h-screen w-full lg:grid-cols-[280px_1fr]">
+      <DashboardSideBar />
+      <DashboardTopNav >
+        <main className="flex flex-col gap-4 p-4 lg:gap-6">
+          {children}
+        </main>
+      </DashboardTopNav>
+    </div>
+  )
+}
