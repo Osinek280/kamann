8321aac1cd5f5ab63068ce1bb4a1be81877e5bd2
Szymon <szymon.osinski.2009@gmail.com>
2025-01-14 15:12:48 +0100

    refactor: test function

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,35 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
+  const handleFetch = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/api/admin/users?page=0&size=1', {
+        method: 'GET',
+        headers: {
+          'accept': '*/*'
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);  
+        })
+        .catch(error => {
+          console.error('Błąd:', error);
+        });
+      
+
+        console.log(response)
+
+    } catch (error) {
+      console.error("Fetch error:", error);
+    }
+  };
+
   return (
-    <p>dashboard - home</p>
+    <Button onClick={handleFetch}>
+      Fetch
+    </Button>
   );
 }
